2ad913f106aea3e12a0ca7ef339e42541fdfc085
mdmahedihassanshuvo <mdmahedihassanshuvo08@gmail.com>
2024-02-11 14:37:12 +0600

    change some style

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <div className="md:w-[1280px] mx-auto">
+      <div className="md:w-[1280px] mx-auto bg-slate-200">
         <RouterProvider router={router} />
       </div>
     </QueryClientProvider>
